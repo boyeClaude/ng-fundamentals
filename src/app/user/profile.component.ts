520a45423999951aa43465842977b01f59dd64db
boye.claude <boye.claude@gmail.com>
2021-11-13 16:59:28 +0000

    reactive profile form with single validators

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,11 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from './login/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.component.html',
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+      .error {
+        background-color: #e3c3c5;
+      }
+    `,
+  ],
 })
 export class ProfileComponent implements OnInit {
-  constructor() {}
+  profileForm!: FormGroup;
+  private firstName!: FormControl;
+  private lastName!: FormControl;
 
-  ngOnInit() {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.firstName = new FormControl(
+      this.authService.currentUser.firstName,
+      Validators.required
+    );
+    this.lastName = new FormControl(
+      this.authService.currentUser.lastName,
+      Validators.required
+    );
+    this.profileForm = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName,
+    });
+  }
+
+  saveProfile(formValues: any) {
+    if (this.profileForm.valid) {
+      this.authService.updateCurrentUser(
+        formValues.firstName,
+        formValues.lastName
+      );
+
+      this.router.navigate(['events']);
+    }
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
+  }
+
+  validateFirstName() {
+    return this.firstName.invalid || this.firstName.touched;
+  }
+
+  validateLastName() {
+    return this.lastName.invalid || this.lastName.touched;
+  }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile.component';
 import { userRoutes } from './user.routes';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(userRoutes), FormsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(userRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   exports: [ProfileComponent, LoginComponent],
   declarations: [ProfileComponent, LoginComponent],
   providers: [],
